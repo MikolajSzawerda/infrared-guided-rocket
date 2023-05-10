fd58d936f7adb90de6d659b1542b62478aaa6407
mszawerd <01169246@pw.edu.pl>
2023-05-10 15:45:39 +0200

    Added plots

--- Lab4/plots.py
+++ Lab4/plots.py
@@ -19,17 +19,34 @@ params = [
 
 fig, ax = plt.subplots(4, 1, figsize=(15, 15))
 for i in range(4):
-
     clients = [
         pd.read_csv(filename, header=None, names=["id", "got", "delivered", "delta"], skiprows=[0])
-        for filename in [f'dane/part3/p{i+1}/{x}' for x in os.listdir(f'dane/part3/p{i+1}') if 'cli' in x]
+        for filename in [f'dane/part4/p{i+1}/{x}' for x in os.listdir(f'dane/part4/p{i+1}') if 'cli' in x]
     ]
     for client in clients:
         ax[i].hist(client['delta'], bins=30)
         ax[i].set_title(params[i])
 fig.suptitle("Czasy opóźnienia klientów")
-fig.savefig("plots/zad4_hist.png")
+# fig.savefig("plots/zad4_hist.png")
 
-# fig2, ax = plt.subplots(1, 1)
-# df = pd.read_csv(f"dane/part3/p4/server.txt", header=None, names=["id", "time", "deltatime"], skiprows=[0])
-# ax[(i, 0)].hist(reject_outliers(df["deltatime"]))
+params2 = [
+    "0 klient z aktywnym oczekiwaniem",
+    "Każdy z aktywnym oczekiwaniem"
+]
+fig2, ax = plt.subplots(2, 3, figsize=(15, 10))
+for i, n in enumerate(['a', 'b']):
+    clients = [
+        pd.read_csv(filename, header=None, names=["id", "got", "delivered", "delta"], skiprows=[0])
+        for filename in sorted([f'dane/part5/{n}/{x}' for x in os.listdir(f'dane/part5/{n}') if 'cli' in x])
+    ]
+    for k, client in enumerate(clients):
+        ax[(i, k)].hist(client["delta"], alpha=0.5)
+        ax[(i, k)].set_title(f'{params2[i]}: klient {k}')
+fig2.suptitle("Zmiana usypiania na aktywne oczekiwanie")
+# fig2.savefig("plots/zad5_hist.png")
+
+fig3, ax = plt.subplots()
+df = pd.read_csv(f"dane/part4/p4/server.txt", header=None, names=["id", "time", "deltatime"], skiprows=[0])
+ax.hist(reject_outliers(df["deltatime"]), bins=30)
+fig3.suptitile("Błędny rozkład czasu serwowania")
+fig3.savefig("plots/zad6_hist.png")
