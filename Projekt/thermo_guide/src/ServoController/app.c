d65ffcbbacf17c605b7d32bfec38f6ab2e137ee2
mszawerd <01169246@pw.edu.pl>
2023-06-07 11:46:28 +0200

    test for servo

--- Projekt/thermo_guide/src/ServoController/app.c
+++ Projekt/thermo_guide/src/ServoController/app.c
@@ -7,10 +7,15 @@ int main(){
 //    sleep(2);
 //    rotate_by_angle(&s, -90.0);
 //    set_servo_rot_direction(&s, IDLE);
-    for(int i=0;i<=180;i+=10){
-        step_set_servo_angle(&s, (double)i);
-        printf("ANGLE: %d\n", i);
-        sleep(2);
-    }
+        for(int i=0;i<100;i++){
+            set_servo_rot_direction(&s, (double) i/100.0);
+            printf("DIR: %f", (double)i/100.0);
+            sleep(2);
+        }
+//    for(int i=0;i<=180;i+=10){
+//        step_set_servo_angle(&s, (double)i);
+//        printf("ANGLE: %d\n", i);
+//        sleep(2);
+//    }
     destroy_servo(&s);
 }
